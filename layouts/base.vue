6d5e1cf91ac780da007380d9997e830836fd6da8
hrfmmymt <hrfmmymt@gmail.com>
2018-08-23 16:43:20 +0900

    first commit

---
+++ layouts/base.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="base">
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+.base {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 100%;
+}
+</style>
